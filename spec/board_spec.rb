a0a21007bc2dee9d988a747ac50e12a5d86bffe7
Kiy Hong Tang <kiyhong@gmail.com>
2014-05-27 21:56:28 +0100

    updated board spec

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -24,8 +24,54 @@ module TicTacToe
     		expect(subject.board).to eq (["-","-","-",
     																	"-","x","-",
     																	"-","-","-"])
+    	end 	
+    end
+
+    describe "winner" do #either 'x', 'o' draw or nil
+    	context "when 'x' is the winner" do 
+    		it "returns x for row" do
+    			subject.add_move("x", 0)
+    			subject.add_move("x", 1)
+    			subject.add_move("x", 2)
+
+    			expect(subject.winner).to eq "x"
+    		end
+
+    		it "returns x for column" do
+    			subject.add_move("x", 0)
+    			subject.add_move("x", 3)
+    			subject.add_move("x", 6)
+
+    			expect(subject.winner).to eq "x"
+    		end
+    	end
+
+    	context "when 'o' is the winner" do 
+    		it "returns o for row" do
+    			subject.add_move("o", 6)
+    			subject.add_move("o", 7)
+    			subject.add_move("o", 8)
+
+    			expect(subject.winner).to eq "o"
+    		end
+
+    		it "returns o for column" do
+    			subject.add_move("o", 2)
+    			subject.add_move("o", 5)
+    			subject.add_move("o", 8)
+
+    			expect(subject.winner).to eq "o"
+    		end
     	end
     end
+
+    describe "game_ended?" do
+    	xit "returns true false"
+    	# based on if all cells filled?
+    	# or there is a winner
+    end
+
+
   end
 end
 
