1436575e201f65d0e9b7eddcee877032274e1b8f
Kiy Hong Tang <kiyhong@gmail.com>
2014-05-27 19:31:03 +0100

    Added Game class

--- lib/tic_tac_toe_ai.rb
+++ lib/tic_tac_toe_ai.rb
@@ -1,4 +1,5 @@
 require "tic_tac_toe_ai/version"
+require "tic_tac_toe_ai/game"
 require "tic_tac_toe_ai/board"
 
 module TicTacToeAi
---
+++ spec/game_spec.rb
@@ -0,0 +1,7 @@
+require "spec_helper"
+
+module TicTacToe
+  describe Game do
+    
+  end
+end
