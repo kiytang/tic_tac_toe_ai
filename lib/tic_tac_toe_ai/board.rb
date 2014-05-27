1c6b97cf752b23dc8b5bf896749ef7bb5f0f170f
Kiy Hong Tang <kiyhong@gmail.com>
2014-05-27 21:58:36 +0100

    Added winner method

--- lib/tic_tac_toe_ai/board.rb
+++ lib/tic_tac_toe_ai/board.rb
@@ -1,13 +1,13 @@
 module TicTacToe
   class Board
     EMPTY = "-"
-    attr_accessor :board, :win_combintions
+    attr_reader :board, :winning_combintions
 
     def initialize
-      @board =[EMPTY] * 9
-      @winning_combintions = [[1,2,3],[4,5,6],[7,8,9], #rows
-                              [1,4,7],[2,5,8],[3,6,7], #columns
-                              [1,5,9],[3,5,7]]         #diagonals  
+      @board = [EMPTY] * 9
+      @winning_combintions = [[0,1,2],[3,4,5],[6,7,8], #rows
+                              [0,3,6],[1,4,7],[2,5,8], #columns
+                              [0,4,8],[2,4,6]]         #diagonals  
     end
 
     def display_board
@@ -24,12 +24,44 @@ module TicTacToe
       @board[number] = marker
     end
 
-    # def choose_marker
-    #   marker == ' '
-    #   while not (marker == 'X' || marker == 'O')
-    #     print('Do you want to be X or O?')
-    #     letter = input().upcase
-    #   end  
-    # end
+    def winner
+      winning_combintions.each do |combo|
+        result = combo.map do |index|
+          x_indexes.include?(index)
+        end.uniq
+
+        win = (result == [true])
+
+        return "x" if win
+      end
+
+      winning_combintions.each do |combo|
+        result = combo.map do |index|
+          o_indexes.include?(index)
+        end.uniq
+
+        win = (result == [true])
+
+        return "o" if win
+      end
+    end
+
+    private
+
+    def o_indexes
+      indexes("o")
+    end
+
+    def x_indexes
+      indexes("x")
+    end
+
+    def indexes(input_marker)
+      board.each_with_index.map do |marker, index|
+        if marker == input_marker
+          index
+        end
+      end.compact
+    end
   end
 end
