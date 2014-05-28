6d4bd3f175cc43801bb0a43af4964f7f1685cfed
Kiy Hong Tang <kiyhong@gmail.com>
2014-05-28 20:21:14 +0100

    Added game_spec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,7 +2,23 @@ require "spec_helper"
 
 module TicTacToe
   describe Game do
-     
+
+    let (:kiy) {Player.new({marker: "x", name: "kiy"})}
+    let (:cpu) {Player.new({marker: "o", name: "cpu"})}
+    
+    context "#initialze" do
+      it "should randomly select a current player" do
+        Array.any_instance.stub(:shuffle) {[kiy, cpu]}
+        game = Game.new([kiy, cpu])
+        expect(game.current_player).to eq kiy
+      end
+
+      it "should randomly select another player" do
+        Array.any_instance.stub(:shuffle) {[kiy, cpu]}
+        game = Game.new([kiy, cpu])
+        expect(game.other_player).to eq cpu  
+      end
+    end
   end
 end
 
