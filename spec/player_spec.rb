7822de16ba556e20fb4c6beab0a11f7e9f8e571a
Kiy Hong Tang <kiyhong@gmail.com>
2014-05-28 12:04:19 +0100

    Added test for both marker and player's name to player_spec

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,12 +1,32 @@
 require "spec_helper"
 module TicTacToe
   describe Player do
-    describe "marker" do
-      subject {described_class.new("x")}
-      it "returns x" do
-        expect(subject.marker).to eq("x") 
+    
+    context "#marker" do
+      it "returns the marker" do
+        input = {marker: "x", name: "Ruby"}
+        player = Player.new(input)
+        expect(player.marker).to eq "x"
       end
     end
+
+    context "#players_name" do
+      it "returns the player's name" do
+        input = {marker: "x", name: "Ruby"}
+        player = Player.new(input)
+        expect(player.name).to eq "Ruby"
+      end
+    end
+
+
+    # describe "marker" do
+    #   subject {described_class.new("x")}
+    #   it "returns x" do
+    #     expect(subject.marker).to eq("x") 
+    #   end
+    # end
+    
+
   end
 end
 
